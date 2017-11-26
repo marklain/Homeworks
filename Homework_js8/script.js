24c1084c033d6614d75381960f4499ecf26682de
Vladislav <marklein.work@gmail.com>
2017-11-26 15:38:13 +0900

    finished homework

--- Homework_js8/script.js
+++ Homework_js8/script.js
@@ -1,9 +1,15 @@
-const endPoint = 'https://api.themoviedb.org/3/search/movie?';
-const apiKey = 'api_key=f24a0fd18f52218851075901c5a108a0';
-const movieSearchForm = document.querySelector('#movie-search');
-const movieName = document.querySelector('#movie-search__name');
-const gallery = document.querySelector('.movie-gallery');
-let info = [];
+const endPointByName =   'https://api.themoviedb.org/3/search/movie?',
+      endPointPopular =  'https://api.themoviedb.org/3/movie/popular?',
+      endPointLatest =   'https://api.themoviedb.org/3/movie/latest?',
+      endPointTopRated = 'https://api.themoviedb.org/3/movie/top_rated?',
+      apiKey =           'api_key=f24a0fd18f52218851075901c5a108a0',
+      movieSearchForm =   document.querySelector('#movie-search'),
+      movieName =         document.querySelector('#movie-search__name'),
+      gallery =           document.querySelector('.movie-gallery'),
+      popularButton =     document.querySelector('.category__popular'),
+      latestButton =      document.querySelector('.category__latest'),
+      topRatedButton =    document.querySelector('.category__top-rated');
+
 
 const renderMovieItem = (parrent, rateText, posterSrc, movieNameText, descriptionText, releaseText) => {
     let div =         document.createElement('div'),
@@ -36,8 +42,8 @@ const renderMovieItem = (parrent, rateText, posterSrc, movieNameText, descriptio
     parrent.appendChild(div);
 }
 
-const fetchMovieList = (input) => {
-    let apiUrl = `${endPoint}${apiKey}&query=${input.value.trim().split(' ').join('+')}`;
+const fetchMovieList = (endPoint, page = '', query = '') => {
+    let apiUrl = `${endPoint}${apiKey}${page}${query}`;
     fetch(apiUrl)
         .then(response => {
             if (response.ok) {
@@ -46,11 +52,30 @@ const fetchMovieList = (input) => {
             throw new Error(`Error while fetching: ${response.statusText}`);
         })
         .then(data => {
-            const movies = data.results;
-            movies.forEach(item => renderMovieItem(gallery, item.vote_average, item.poster_path, item.title, item.overview, item.release_date))
-        })
+            if (data.results != undefined) {
+                const movies = data.results;
+                movies.forEach(item => renderMovieItem(gallery, item.vote_average, item.poster_path, item.title, item.overview, item.release_date));
+            } else {
+                renderMovieItem(gallery, data.vote_average, data.poster_path, data.title, data.overview, data.release_date);
+            }
+            })
         .catch(err => console.log(err));
 };
+const fetchPopularMovieList = () => {
+    let apiUrl = `${endPointPopular}${apiKey}&page=1`;
+    fetch(apiUrl)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(`Error while fetching: ${response.statusText}`);
+        })
+        .then(data => {
+            let result = data.results;
+            result.forEach(item => renderMovieItem(gallery, item.vote_average, item.poster_path, item.title, item.overview, item.release_date))
+        })
+        .catch(err => console.log(err));
+}
 
 const clearMoiveList = () => {
     document.querySelectorAll('.movie-card').forEach((elem) => {
@@ -59,10 +84,26 @@ const clearMoiveList = () => {
 
 }
 
+
+
 movieSearchForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    let query = `&query=${movieName.value.trim().split(' ').join('+')}`;
 
     clearMoiveList();
-    fetchMovieList(movieName);
+    fetchMovieList(endPointByName, '', query);
     movieName.value = '';
+});
+
+popularButton.addEventListener('click', (e) => {
+    clearMoiveList();
+    fetchMovieList(endPointPopular, '&page=1');
+});
+latestButton.addEventListener('click', (e) => {
+    clearMoiveList();
+    fetchMovieList(endPointLatest, '&page=1');
+});
+topRatedButton.addEventListener('click', (e) => {
+    clearMoiveList();
+    fetchMovieList(endPointTopRated, '&page=1');
 });
