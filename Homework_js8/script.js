d17a4f3508f9fec8cfa59ae3a4fe5702ce9b8ad4
Vladislav <marklein.work@gmail.com>
2017-11-28 22:49:34 +0900

    reworked

--- Homework_js8/script.js
+++ Homework_js8/script.js
@@ -11,34 +11,15 @@ const endPointByName = 'https://api.themoviedb.org/3/search/movie?',
     topRatedButton = document.querySelector('.category__top-rated');
 
 const renderMovieItem = (parrent, rateText, posterSrc, movieNameText, descriptionText, releaseText) => {
-    let div = document.createElement('div'),
-        rate = document.createElement('p'),
-        poster = document.createElement('img'),
-        movieName = document.createElement('p');
-    description = document.createElement('p'),
-        release = document.createElement('p');
-    div.className = 'movie-card';
-    rate.className = 'movie-card__rate';
-    rate.innerHTML = `Rating: ${rateText}`;
-
-    poster.className = 'movie-card__poster';
-    poster.src = `https://image.tmdb.org/t/p/w500/${posterSrc}`;
-
-    movieName.innerHTML = movieNameText;
-    movieName.className = 'movie-card__name'
-
-    description.className = 'movie-card__description';
-    description.innerHTML = `${descriptionText.slice(0, 99)}...`;
-
-    release.className = 'movie-card__release';
-    release.innerHTML = `Release: ${releaseText}`;
-
-    div.appendChild(rate);
-    div.appendChild(poster);
-    div.appendChild(movieName)
-    div.appendChild(description);
-    div.appendChild(release);
-    parrent.appendChild(div);
+    let templateString =
+        `<div class="movie-card">
+            <p class="movie-card__rate">Rating: ${rateText}</p>
+            <img class="movie-card__poster" src="https://image.tmdb.org/t/p/w500/${posterSrc}">
+            <p class="movie-card__name">${movieNameText}</p>
+            <p class="movie-card__description">${descriptionText.slice(0, 99)}...</p>
+            <p class="movie-card__release">Release: ${releaseText}</p>
+        </div>`;
+    parrent.innerHTML += templateString;
 }
 
 const fetchMovieList = (endPoint, page = '', query = '') => {
@@ -51,7 +32,7 @@ const fetchMovieList = (endPoint, page = '', query = '') => {
             throw new Error(`Error while fetching: ${response.statusText}`);
         })
         .then(data => {
-            if (data.results != undefined) {
+            if (data.results !== undefined) {
                 const movies = data.results;
                 movies.forEach(item => renderMovieItem(gallery, item.vote_average, item.poster_path, item.title, item.overview, item.release_date));
             } else {
@@ -62,10 +43,7 @@ const fetchMovieList = (endPoint, page = '', query = '') => {
 };
 
 const clearMoiveList = () => {
-    document.querySelectorAll('.movie-card').forEach((elem) => {
-        elem.remove();
-    });
-
+    document.querySelector('.movie-gallery').innerHTML = '';
 }
 
 movieSearchForm.addEventListener('submit', (e) => {
