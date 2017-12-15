415ebab5d0577f346d50f5a1c863602f1a68cdf2
Vladislav <marklein.work@gmail.com>
2017-12-15 06:31:20 +0200

    rewrited keys by v4

--- hw-react_1/src/movies.js
+++ hw-react_1/src/movies.js
@@ -1,34 +1,35 @@
+import v4 from 'uuid/v4';
 const movies = [
     {
-        id: '001',
+        id: v4(),
         title: 'Star Wars',
         descr: 'The Imperial Forces -- under orders from cruel Darth Vader (David Prowse) -- hold Princess Leia (Carrie Fisher) hostage, in their efforts to quell the rebellion against the Galactic Empire. Luke Skywalker (Mark Hamill) and Han Solo (Harrison Ford), c...',
         rating: '7/10',
         genres: ['sci-fi', 'drama', 'action']
     },
     {
-        id: '002',
+        id: v4(),
         title: 'Jaws',
         descr: 'When a young woman is killed by a shark while skinny-dipping near the New England tourist town of Amity Island, police chief Martin Brody (Roy Scheider) wants to close the beaches, but mayor Larry Vaughn (Murray Hamilton) overrules him, fearing that ...',
         rating: '6/10',
         genres: ['drama', 'action', 'horror']
     },
     {
-        id: '003',
+        id: v4(),
         title: 'Conan',
         descr: 'Orphaned boy Conan (Arnold Schwarzenegger) is enslaved after his village is destroyed by the forces of vicious necromancer Thulsa Doom (James Earl Jones), and is compelled to push "The Wheel of Pain" for many years. Once he reaches adulthood, Conan s...',
         rating: '7/10',
         genres: ['action']
     },
     {
-        id: '004',
+        id: v4(),
         title: 'The House',
         descr: 'Scott and Kate Johansen must figure out a way to earn some money after their daughters scholarship falls through. When all else fails, the desperate couple join forces with their neighbor Frank to start an underground casino in his home. As the cash...',
         rating: '5/10',
         genres: ['comedy']
     },
     {
-        id: '005',
+        id: v4(),
         title: 'Training Day',
         descr: 'Police drama about a veteran officer who escorts a rookie on his first day with the LAPDs tough inner-city narcotics unit. "Training Day" is a blistering action drama that asks the audience to decide what is necessary, what is heroic and what crosse...',
         rating: '9/10',
