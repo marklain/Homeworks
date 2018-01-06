664c47ee6b915306c29357e9c65b4bc16a158723
Vladislav <marklein.work@gmail.com>
2018-01-06 23:30:08 +0200

    added basename to browserRouter

--- hw-react_3/src/components/Root/index.jsx
+++ hw-react_3/src/components/Root/index.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
 
 const Root = () => (
-    <BrowserRouter>
+    <BrowserRouter basename="Homeworks/hw-react_3/build/">
         <App />
     </BrowserRouter>
 );
