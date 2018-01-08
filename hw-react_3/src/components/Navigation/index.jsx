f51540a4382dd6081fc5855abb08322029ce8ca2
Vladislav <marklein.work@gmail.com>
2018-01-08 19:20:09 +0200

    debudeg NavLink by exact

--- hw-react_3/src/components/Navigation/index.jsx
+++ hw-react_3/src/components/Navigation/index.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = (props) => (
     <nav className="Navigation">
-        <NavLink to="/" activeClassName="Navigation__link--active" className="Navigation__link">Home</NavLink>
+        <NavLink exact to="/" activeClassName="Navigation__link--active" className="Navigation__link">Home</NavLink>
         <NavLink to="/movies" activeClassName="Navigation__link--active" className="Navigation__link">movies</NavLink>
         <NavLink to="/about" activeClassName="Navigation__link--active" className="Navigation__link">about</NavLink>
     </nav>
